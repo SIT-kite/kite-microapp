1c1138869be6fbea53b6fdb9529d6ba3cbcabc5c
Peanuts <849421294godw@gmail.com>
2020-09-04 18:50:02 +0800

    修改新朋友页面逻辑

--- pages/freshman/newFriend/newFriend.js
+++ pages/freshman/newFriend/newFriend.js
@@ -1,6 +1,7 @@
 // pages/newFriend/newFriend.js
 var app = getApp();
 import { handlerGohomeClick, handlerGobackClick } from '../../../utils/navBarUtils'
+const util = require("../../../utils/utils.js");
 Page({
 
   /**
@@ -18,7 +19,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var util = require("../../../utils/utils.js");
+
     console.log("onload");
     var that = this;
     if (app.globalData.roommates == null) {
@@ -33,10 +34,10 @@ Page({
       });
       // 获取室友信息
       wx.request({
-        url: `${app.globalData.commonUrl}/freshman/${app.globalData.userInfo.name_examNumber}/roommate`,
+        url: `${app.globalData.commonUrl}/freshman/${app.globalData.userInfo.account}/roommate`,
         method: "GET",
         data: {
-          "secret": `${app.globalData.userInfo.id}`
+          "secret": `${app.globalData.userInfo.secret}`
         },
         header: {
           "content-type": "application/x-www-form-urlencoded",
@@ -44,14 +45,21 @@ Page({
         },
         success(res) {
           if (res.data.code == 0) {
-            that.data.roommates = res.data.data.roommates;
+            console.log("roomate 获取成功");
+            // that.data.roommates = res.data.data.roommates;
+
+            that.setData({
+              roommates:  res.data.data.roommates
+            });
+            let url =`${app.globalData.commonUrl}/freshman/${app.globalData.userInfo.account}/familiar`;
+            console.log(`url: ${url}`); 
             // 获取可能认识的人
-            if (app.globalData.visible) {
+            if (app.globalData.userInfo.visible) {
               wx.request({
-                url: `${app.globalData.commonUrl}/freshman/${app.globalData.userInfo.name_examNumber}/familiar`,
+                url: url,
                 method: "GET",
                 data: {
-                  "secret": `${app.globalData.userInfo.id}`
+                  "secret": `${app.globalData.userInfo.secret}`
                 },
                 header: {
                   "content-type": "application/x-www-form-urlencoded",
@@ -59,8 +67,8 @@ Page({
                 },
                 success(res) {
                   // console.log(res.data);
+                  console.log("familiar 获取成功")
                   if (res.data.code == 0) {
-                    wx.hideLoading();
                   }
                   else {
                     wx.showModal({
@@ -78,7 +86,8 @@ Page({
                     showCancel: false,
                     success(res) { }
                   })
-                }
+                },
+                complete: ()=>{wx.hideLoading();}
               })
             }
             // 不用获取可能认识的人
@@ -86,8 +95,8 @@ Page({
               for (var i = 0; i < that.data.roommates.length; i++) {
                 that.data.roommates[i].lastSeen = util.getIntervalToCurrentTime(that.data.roommates[i].lastSeen);
                 that.data.roommates[i].isHidden = {
-                  "qq": null,
-                  "wechat": null
+                  qq: null,
+                  wechat: null
                 }
                 if (that.data.roommates[i].contact == null) {
                   that.data.roommates[i].isHidden.qq = true;
@@ -101,11 +110,14 @@ Page({
               // console.log(that.data.roommates[0].lastSeen);
               // console.log(util.getIntervalToCurrentTime(that.data.roommates[0].lastSeen));
               // console.log(that.data.roommates);
-              that.setData({
-                roommates: that.data.roommates
-              })
-              app.globalData.roommates = res.data.data.roommates;
+
             }
+            // 无论 if else如何都需要存入全局变量
+            that.setData({
+              roommates: that.data.roommates
+            })
+            app.globalData.roommates = res.data.data.roommates;
+            wx.hideLoading();
           } else {
             wx.showModal({
               title: "哎呀，出错误了>.<",
@@ -121,12 +133,14 @@ Page({
             content: "网络不在状态",
             showCancel: false,
             success(res) { }
-          })
-        }
+          });
+        },
+        complete: () => {},
       })
     }
     // 本地有可能认识人和室友的信息
     else {
+      console.log("本地已有信息！");
       that.setData({
         roommates: app.globalData.roommates,
         familiar: app.globalData.familiar
