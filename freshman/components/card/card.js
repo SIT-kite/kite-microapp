80826165fb8b7cfcf2b77055f75c43d47f7edfad
sunnysab <sunnysab@yeah.net>
2021-06-14 17:13:17 +0800

    更新迎新模块, 组件化展示卡片
    1. 组件化展示卡片
    2. 将卡片应用于 "新的班级" 页面
    
    本 commit 引入了一些 bug, 如 "新的班级" 页面显示异常, 经过分析这可能是变量的生命周期和更新顺序导致的, 需要在后续版本中修复

---
+++ freshman/components/card/card.js
@@ -0,0 +1,96 @@
+// freshman/components/card.js
+
+const timeUtils = require("../../../utils/timeUtils");
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 姓名
+    name: {
+      type: String,
+    },
+    // 好友的 QQ 号
+    qq: {
+      type: String,
+      value: null
+    },
+    // 好友的微信号
+    wechat: {
+      type: String,
+      value: null
+    },
+    // 好友的手机号
+    telphone: {
+      type: String,
+      value: null
+    },
+    // 好友的头像
+    avatarImage: {
+      type: String
+    },
+    // 来自的区域, 具体内容取决于数据源, 可能是省、市或精确到县、乡
+    region: {
+      type: String
+    },
+    // 性别. M 表示 male, F 表示 Female
+    gender: {
+      type: String
+    },
+    // 宿舍楼号, 本条目仅在 "新的班级" 页面展示
+    // 和 "寝室号" 二选一
+    building: {
+      type: String,
+      value: null,
+    },
+    // 寝室号, 默认值为空. 该字段仅在 "室友查询（新的朋友）" 页需要填写
+    // 与 "宿舍楼号" 使用同一位置
+    roomNumber: {
+      type: String,
+      value: ''
+    },
+    // 该用户上次登录的时间戳
+    lastSeen: {
+      type: String,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    lastSeenText: null,
+    genderImage: null,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    /**
+     * 根据性别返回性别图标的路径
+     * @param { String } gender 性别. "M" 为男, "F" 为女
+     */
+    _getGenderIconPath: function(gender) {
+      if (gender === "M")
+        return '/freshman/assets/male.png';
+      else if (gender === "F")
+        return '/freshman/assets/female.png';
+
+      console.error(`Invalid gender string ${gender} passed to _getGenderIconPath`);
+    }
+  },
+
+  /**
+   * 组件创建时的响应事件
+   */
+  created: function() {
+    this.setData({
+      // 根据该用户上次访问时间, 算出时间差, 并转换成字符串
+      lastSeenText: timeUtils.getIntervalToCurrentTime(this.properties.lastSeen),
+      // 根据性别数据得到对应的 icon
+      genderImage: this._getGenderIconPath(this.properties.gender),
+    });
+  }
+})
--- freshman/pages/newClass/newClass.js
+++ freshman/pages/newClass/newClass.js
@@ -1,10 +1,12 @@
 // pages/newClass/newClass.js
-import { handlerGohomeClick, handlerGobackClick } from "../../../utils/navBarUtils";
+import {
+  handlerGohomeClick,
+  handlerGobackClick
+} from "../../../utils/navBarUtils";
 import copyText from "../../../utils/copyText";
 import catchError from "../../../utils/requestUtils.catchError";
 
 const utlls = "../../../utils/";
-const timeUtils = require(utlls + "timeUtils");
 const requestUtils = require(utlls + "requestUtils");
 
 const app = getApp();
@@ -12,84 +14,51 @@ const app = getApp();
 Page({
 
   data: {
-    classmates: null,
-    show: false
+    classmates: [],
   },
 
   handlerGohomeClick,
   handlerGobackClick,
-
   copyText,
 
-  // 初始化页面 classmates 数据
-  pageDataInit: function () {
+  /**
+   * 请求班级同学信息
+   */
+  async _requestClassmateList() {
+    let url = `${app.globalData.commonUrl}/freshman/${app.globalData.userInfo.account}/classmate`;
+    let data = {
+      "secret": `${app.globalData.userInfo.secret}`
+    };
+    let header = {
+      "content-type": "application/x-www-form-urlencoded",
+      "Authorization": `Bearer ${app.globalData.token}`,
+    };
 
-    let url = "";
-    let data = {};
-    let header = {};
+    let  response = await requestUtils.doGET(url, data, header);
+    let  responsedData = response.data;
+    let  classmateList = responsedData.data.classmates;
 
-    if (app.globalData.classmates == null) {
-      // 判断全局是否包含 classmates 数据
-      console.log("请求远程 classmates 数据");
-      wx.showLoading({
-        title: '加载中',
-        mask: true
-      });
+    return classmateList;
+  },
 
-      url = `${app.globalData.commonUrl}/freshman/${app.globalData.userInfo.account}/classmate`;
-      data = {
-        "secret": `${app.globalData.userInfo.secret}`
-      };
-      header = {
-        "content-type": "application/x-www-form-urlencoded",
-        "Authorization": `Bearer ${app.globalData.token}`,
-      };
-      var getClassmates = requestUtils.doGET(url, data, header).then(res => {
-        var stuList = res.data.data.classmates;
-        stuList.forEach(student => {
-
-          student.genderImage = "/freshman/assets/" + (
-            student.gender === "M"
-            ? "male.png"
-            : "female.png"
-          );
-          student.lastSeen = timeUtils.getIntervalToCurrentTime(student.lastSeen);
-          student.isHidden = {
-            "qq": null,
-            "wechat": null,
-            "padding": null
-          }
-
-          if (student.contact == null) {
-            student.isHidden.qq = true;
-            student.isHidden.wechat = true;
-          }
-          else {
-            student.isHidden.qq = student.contact.qq === "";
-            student.isHidden.wechat = student.contact.wechat === "";
-            student.isHidden.padding = student.isHidden.wechat === true ? 25 : 0;
-          }
-        });
-        app.globalData.classmates = res.data.data.classmates;
-        this.setData({ classmates: stuList });
-        wx.hideLoading();
-        return res;
-      });
-
-      getClassmates.then(
-        () => console.log("请求加载完成")
-      ).catch(catchError);
-
-      // 阻塞页面渲染
-      Promise.all([getClassmates]).then(() => {
-        console.log("数据加载完成");
-        this.setData({show: true})
-      });
-    } else {
-      console.log("加载本地 classmates 数据");
-      this.setData({
-        classmates: app.globalData.classmates,
-        show: true
+  /**
+   * 请求班级同学信息并更新页面. 请求出错时弹出错误提示.
+   */
+  async _getClassmates() {
+    try {
+      // 直接返回接口更新的列表
+      return await this._requestClassmateList();
+    }
+    catch(error) {
+      // 请求出现网络错误, 弹窗提示
+      wx.showModal({
+        title: "哎呀，出错误了 >.<",
+        content: (
+          res.error == requestUtils.REQUEST_ERROR ? res.data
+          : res.error == requestUtils.NETWORK_ERROR ? "网络不在状态"
+          : "未知错误"
+        ),
+        showCancel: false
       });
     }
   },
@@ -97,106 +66,32 @@ Page({
   /**
    * 页面数据刷新
    */
-  pageDataFresh: function () {
-    this.setData({show: false});
-    let url = "";
-    let data = {};
-    let header = {};
-
-    url = `${app.globalData.commonUrl}/freshman/${app.globalData.userInfo.account}/classmate`;
-    data = {
-      "secret": `${app.globalData.userInfo.secret}`
-    };
-    header = {
-      "content-type": "application/x-www-form-urlencoded",
-      "Authorization": `Bearer ${app.globalData.token}`,
-    };
-
-    var refleshClassmates = requestUtils.doGET(url, data, header).then(res => {
-      var stuList = res.data.data.classmates;
-      stuList.forEach(student => {
-        student.genderImage = student.gender === "M" ? "/freshman/assets/male.png" : "/freshman/assets/female.png";
-        student.lastSeen = timeUtils.getIntervalToCurrentTime(student.lastSeen);
-        student.isHidden = {
-          "qq": null,
-          "wechat": null,
-          "padding": null
-        }
-        if (student.contact == null) {
-          student.isHidden.qq = true;
-          student.isHidden.wechat = true;
-        }
-        else {
-          student.isHidden.qq = student.contact.qq === "";
-          student.isHidden.wechat = student.contact.wechat === "";
-          student.isHidden.padding = student.isHidden.wechat === true ? 25 : 0;
-        }
-      });
-      app.globalData.classmates = res.data.data.classmates;
-      this.setData({
-        classmates: stuList
-      });
-      wx.hideLoading();
-      return res;
+  async onPullDownRefresh () {
+    // 加载数据
+    this.setData({
+      classmates: await this._getClassmates()
     });
-    refleshClassmates.then(() => {
-      // 关闭刷新动画
-      this.setData({show: true});
-      this.onShow();
-    }).catch(catchError);
+    // 关闭刷新动画
+    wx.stopPullDownRefresh();
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function () {
+  async onLoad () {
     console.log("页面 newClass onLoad...");
-    this.setData({show: false});
-    this.pageDataInit();
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    wx.stopPullDownRefresh();
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    console.log("页面 newClass 刷新中");
-    this.pageDataFresh();
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+    
+    // 显示加载提示框
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    });
+    // 加载并设置数据
+    this.setData({
+      classmates: await this._getClassmates()
+    });
+    // 取消加载提示框
+    wx.hideLoading();
   },
 
   /**
@@ -204,8 +99,8 @@ Page({
    */
   onShareAppMessage: function () {
     return {
-      title: "上应小风筝",
-      path: "pages/index/index"
+      title: "在上应小风筝开启大学生活第一站！",
+      path: "/freshman/pages/welcome"
     }
   }
 })
