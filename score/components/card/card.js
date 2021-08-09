6504d71723d6f659096d48b40c07c09f90ef9ca7
Pony-Zhang <sherly_cs_sh@163.com>
2021-08-09 14:41:20 +0800

    添加查成绩模块
    1.添加查成绩页面
    2.创建自定义卡片

---
+++ score/components/card/card.js
@@ -0,0 +1,31 @@
+// score/components/card/card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    name:{
+      type:String,
+    },
+    credit: {
+      type:String,
+    },
+    grades: {
+      type:String
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
