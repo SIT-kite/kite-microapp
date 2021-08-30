dae8e9c300a1b4238a0593968abfa2307ce1f047
zdy180108 <zdytim@foxmail.com>
2021-08-31 00:32:13 +0800

    优化 transformationsUtils 代码

--- utils/transformationsUtils.js
+++ utils/transformationsUtils.js
@@ -1,18 +1,14 @@
-// transformations(times : number,length : number) Point:array
+// transformations(number : number,length : number) Point:array
 // 二进制转换，并返回成数组(length为想要数组长度)
 // 返回值格式[1,0,1,0]
-const transformations = (times,length) => {
-    　　var two = parseInt(times).toString(2);
-    　　var Point = [];
-    　　if (two.length < length) {
-    　　　　for (var j = 0; j < (length - two.length); j++) {
-    　　　　　　Point.push('0')
-    　　　　}
-    　　}
-    　　for (var k = 0; k < two.length; k++) {
-    　　　　Point.push(two.charAt(k))
-    　　}
-    　　return Point;
+const transformations = (number,length) => {
+    var result = [];
+    for (var  i = 1; i < length+1; i++){
+        if (number & (1 << i)) {
+        result.push(1);
+        }else{result.push(0)}
+        }
+    return result;
     }
 
 
