b1b566976a2f81b130a22ce3a6240c1cc963c22e
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2021-09-01 14:14:51 +0800

    格式化成绩代码

--- score/pages/index/index.js
+++ score/pages/index/index.js
@@ -1,8 +1,5 @@
 // score/pages/index/index.js
-import {
-  handlerGohomeClick,
-  handlerGobackClick
-} from '../../../utils/navBarUtils'
+import { handlerGohomeClick, handlerGobackClick } from "../../../utils/navBarUtils";
 import onShareAppMessage from "../../../utils/onShareAppMessage";
 import getHeader from "../../../utils/getHeader";
 
@@ -13,29 +10,26 @@ const header = getHeader("urlencoded", app.globalData.token);
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
     tipText: '已经是最新了哦!',
-    termList:[],
-    yearList:[],
+    termList: [],
+    yearList: [],
     isFullTerm: true,
     isReferred: false,
     isYearGpa: false,
     isShowText: false,
     scorePageInfo: {
       currentScoreList: [],
-      totalScoreList:[],
+      totalScoreList: [],
       termGpa: null,
       yearGpa: null,
       termIndex: 0,
       yearIndex: 0,
     },
-    
+
   },
 
-  //获取用户学号
+  // 获取用户学号
   // getStudentId() {
   //   let data = {}
   //   let url = `${app.globalData.apiUrl}/user/${app.globalData.uid}/identity`
@@ -55,37 +49,37 @@ Page({
   //   })
   // },
 
-  //计算并设置初始学年和学期数组
+  // 计算并设置初始学年和学期数组
   caculateYearArr() {
     let date = new Date()
     let startYear = parseInt(wx.getStorageSync('identity').studentId.slice(0, 2))
     let nowYear = parseInt(date.getFullYear().toString().slice(2, 4))
     let nowMonth = date.getMonth()
-    let termInterval = parseInt(((nowYear - startYear) > 1? (nowYear - startYear) - 1: 0) * 2 + (nowMonth + 4) / 6 + 1)
+    let termInterval = parseInt(((nowYear - startYear) > 1 ? (nowYear - startYear) - 1 : 0) * 2 + (nowMonth + 4) / 6 + 1)
     let yearInterval = parseInt(termInterval / 2)
-    if(termInterval % 2) this.setData({isFullTerm: false})
+    if (termInterval % 2) this.setData({isFullTerm: false})
 
     let baseSring = '20'
     let yearList = []
-    for(let i = 0, year = startYear; i < yearInterval; i++, year++) {
+    for (let i = 0, year = startYear; i < yearInterval; i++, year++) {
       yearList.push(`${baseSring}${year}-${baseSring}${year + 1}`)
     }
-    this.setData({yearList: yearList})
+    this.setData({yearList})
     this.setData({'scorePageInfo.yearIndex': yearList.length - 1})
-    if(this.data.isFullTerm) {
-      this.setData({termList: ['第一学期','第二学期']})
-    }else {
+    if (this.data.isFullTerm) {
+      this.setData({termList: ['第一学期', '第二学期']})
+    } else {
       this.setData({termList: ['第一学期']})
     }
 
     console.log(wx.getStorageSync('identity').studentId)
   },
 
-  //计算绩点
+  // 计算绩点
   getGPA(scoreList) {
     let totalCredits = 0.0
     let t = 0.0
-    for(let s of scoreList) {
+    for (let s of scoreList) {
       t += s.credit * s.score
       totalCredits += s.credit
     }
@@ -94,26 +88,26 @@ Page({
 
   onSelectChange(e) {
     // console.log(this.data.yearList.length)
-    if(e.currentTarget.dataset.index_name == "yearIndex") {
+    if (e.currentTarget.dataset.index_name === "yearIndex") {
       this.setData({'scorePageInfo.yearIndex': e.detail.value})
-      
-      if(e.detail.value == this.data.yearList.length - 1 && !this.data.isFullTerm) {
+
+      if (e.detail.value == this.data.yearList.length - 1 && !this.data.isFullTerm) {
         this.setData({termList: ['第一学期'], termIndex: 0})
-      }else {
-        this.setData({termList: ['第一学期','第二学期']})
+      } else {
+        this.setData({termList: ['第一学期', '第二学期']})
       }
-    }else {
+    } else {
       this.setData({'scorePageInfo.termIndex': e.detail.value})
     }
     // console.log(this.data.termIndex, this.data.yearIndex)
   },
 
-  //切换学期和学年绩点
+  // 切换学期和学年绩点
   switchGpa(e) {
     this.setData({isYearGpa: e.detail.value})
   },
 
-  //获取成绩列表
+  // 获取成绩列表
   getScoreList() {
     let identityUrl = `${app.globalData.apiUrl}/user/${app.globalData.uid}/identity`
     let getVerified = requestUtils.doGET(identityUrl, {}, header)
@@ -122,11 +116,9 @@ Page({
         wx.setStorageSync('verified', false)
         app.globalData.identity = {}
         wx.setStorageSync('identity', {})
-        wx.redirectTo({
-          url: '/pages/verify/verify',
-        })
+        wx.redirectTo({ url: '/pages/verify/verify' })
     })
-    
+
     // console.log(url)
     let yearList = this.data.yearList
     let yearIndex = this.data.scorePageInfo.yearIndex
@@ -134,32 +126,44 @@ Page({
     let data = {}
     let getData = requestUtils.doGET(url, data, header)
     getData.then((res) => {
+
       this.setData({isReferred: true})
-      if(res.data.data.Score[0] != null) {
+
+      if (res.data.data.Score[0] != null) {
+
         this.setData({"scorePageInfo.yearGpa": this.getGPA(res.data.data.Score).toFixed(2)})
         this.setData({"scorePageInfo.totalScoreList": res.data.data.Score})
+
         let currentScoreList = []
         res.data.data.Score.forEach((item) => {
-          if(parseInt(item.semester) == parseInt(this.data.scorePageInfo.termIndex) + 1) {
+          if (parseInt(item.semester) == parseInt(this.data.scorePageInfo.termIndex) + 1) {
             currentScoreList.push(item)
           }
         })
-        if(JSON.stringify(this.data.scorePageInfo.currentScoreList) == JSON.stringify(currentScoreList)) {
+
+        if (JSON.stringify(this.data.scorePageInfo.currentScoreList) == JSON.stringify(currentScoreList)) {
           this.setData({tipText: '已经是最新了哦!'})
           this.setData({isShowText: true})
           setTimeout(() => this.setData({isShowText: false}), 2000)
-        }else {
-          this.setData({tipText: this.data.scorePageInfo.currentScoreList[0] == null ||this.data.scorePageInfo.currentScoreList[0].semester != currentScoreList[0].semester? '查询成功' : '刷新成功'})
+        } else {
+          this.setData({
+            tipText:
+              this.data.scorePageInfo.currentScoreList[0] == null ||
+              this.data.scorePageInfo.currentScoreList[0].semester != currentScoreList[0].semester ? '查询成功' : '刷新成功'
+          })
           this.setData({isShowText: true})
           setTimeout(() => this.setData({isShowText: false}), 2000)
           this.setData({"scorePageInfo.currentScoreList": currentScoreList})
           this.setData({"scorePageInfo.termGpa": this.getGPA(currentScoreList).toFixed(2)})
         }
+
         wx.setStorageSync('scorePageInfo', this.data.scorePageInfo)
-      }else {
+
+      } else {
+
         let scorePageInfo = {
           currentScoreList: [],
-          totalScoreList:[],
+          totalScoreList: [],
           termGpa: null,
           yearGpa: null,
           termIndex: 0,
@@ -168,15 +172,17 @@ Page({
         wx.setStorageSync('scorePageInfo', scorePageInfo)
         scorePageInfo.termIndex = this.data.scorePageInfo.termIndex
         scorePageInfo.yearIndex = this.data.scorePageInfo.yearIndex
-        this.setData({scorePageInfo: scorePageInfo})
+        this.setData({scorePageInfo})
+
       }
+
     }).catch((res) => {
       console.log(res)
-      if(app.globalData.verified) {
+      if (app.globalData.verified) {
         wx.showModal({
           cancelColor: 'cancelColor',
           content: "获取成绩失败，请联系开发人员",
-          showCancel:false
+          showCancel: false
         })
       }
     })
@@ -185,75 +191,23 @@ Page({
 
   handlerGohomeClick,
   handlerGobackClick,
+  onShareAppMessage,
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    if(!app.globalData.verified) {
-      wx.redirectTo({
-        url: '/pages/verify/verify',
-      })
+  onLoad () {
+
+    if (!app.globalData.verified || !wx.getStorageSync('identity').studentId) {
+      wx.redirectTo({ url: '/pages/verify/verify' })
     }
-    if(!wx.getStorageSync('identity').studentId){
-      wx.redirectTo({
-        url: '/pages/verify/verify',
-      })
-    }
-    
+
     this.caculateYearArr()
-    if(wx.getStorageSync('scorePageInfo').currentScoreList[0]) {
+
+    if (wx.getStorageSync('scorePageInfo').currentScoreList[0]) {
       this.setData({isReferred: true})
       this.setData({scorePageInfo: wx.getStorageSync('scorePageInfo')})
     }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // onReady () {},
+  // onShow () {},
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
