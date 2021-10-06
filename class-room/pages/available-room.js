39ab3bfbdf31d6388d1f913bc7613e6e767f4fea
zdy180108 <zdytim@foxmail.com>
2021-10-06 23:14:18 +0800

    更新 空教室
    1.:art:重构代码
    2.:zap:调整空教室依赖删除冗余代码
    3.:heavy_plus_sign:timeUtils中添加函数

--- class-room/pages/available-room.js
+++ class-room/pages/available-room.js
@@ -1,220 +1,153 @@
-const dayjs = require('../components/dayjs/index')
-
 import {
   handlerGohomeClick,
-  handlerGobackClick
-} from '../../utils/navBarUtils';
+  handlerGobackClick,
+} from "../../utils/navBarUtils";
 import getHeader from "../../utils/getHeader";
 
-const app =  getApp();
+const app = getApp();
 const availableSuffix = `/edu/classroom/available`;
 const requestUtils = require("../../utils/requestUtils");
-const transformationsUtils = require("../../utils/transformationsUtils") 
-const promisify = require('../../utils/promisifyUtils');
+const transformationsUtils = require("../../utils/transformationsUtils");
+const timeUtils = require("../../utils/timeUtils");
 
-
-const timeIntervals = {
-  1: [{h: 6, m: 0}, {h: 8, m: 45}],
-  2: [{h: 8, m: 45}, {h: 9, m: 40}],
-  3: [{h: 9, m: 40}, {h: 10, m: 40}],
-  4: [{h: 10, m: 40}, {h: 11, m: 35}],
-  5: [{h: 11, m: 35}, {h: 12, m: 30}],
-  6: [{h: 12, m: 30}, {h: 14, m: 15}],
-  7: [{h: 14, m: 15}, {h: 15, m: 10}],
-  8: [{h: 15, m: 10}, {h: 16, m: 10}],
-  9: [{h: 16, m: 10}, {h: 17, m: 5}],
-  10: [{h: 17, m: 5}, {h: 18, m: 0}],
-  11: [{h: 18, m: 0}, {h: 19, m: 15}],
-  12: [{h: 19, m: 15}, {h: 20, m: 10}],
-  13: [{h: 20, m: 10}, {h: 21, m: 5}],
-  14: [{h: 21, m: 5}, {h: 22, m: 30}]
-}
-
-const campuses = ["奉贤校区", "徐汇校区"]
-let choosedCampus = "奉贤校区"
+const campuses = ["奉贤校区", "徐汇校区"];
+let choosedCampus = "奉贤校区";
 
 const buildings = {
-  "奉贤校区": ["A", "B","C","D","E","F","G","H","I"],
-  "徐汇校区": ["教学楼","南图"]
-}
+  奉贤校区: ["A", "B", "C", "D", "E", "F", "G", "H", "I"],
+  徐汇校区: ["教学楼", "南图"],
+};
 
-let choosedBuilding= "A"
-let data_content = {}
-
+let choosedBuilding = "A";
+let data_content = {};
+let content = {};
 Page({
-  data: { campuses, choosedCampus, buildings, choosedBuilding, hideBottom: true ,index:0,data_content},
+  data: {
+    campuses,
+    choosedCampus,
+    buildings,
+    choosedBuilding,
+    hideBottom: true,
+    index: 1,
+    content,
+    data_content,
+  },
 
   // 导航栏函数
   handlerGohomeClick: handlerGohomeClick,
   handlerGobackClick: handlerGobackClick,
 
-  setcampuses: function (){
-    return this.data.choosedCampus;
-  },
-  
-  Choosed: function(){
+  Choosed: function () {
     let campus;
     let building;
     if (this.data.choosedCampus == "奉贤校区") {
-      campus = 1, building = "region";
-    } else if(this.data.choosedCampus == "徐汇校区")
-    { campus = 2, building = "building";}
-    return [campus,building];
+      (campus = 1), (building = "region");
+    } else if (this.data.choosedCampus == "徐汇校区") {
+      (campus = 2), (building = "building");
+    }
+    return [campus, building];
   },
-
-  setdata: function(Campus,building,index,date){
-    let _this=this
+  //发送请求
+  sendData(Campus, building, index, date) {
+    let _this = this;
     let available = `?campus=${Campus}&date=${date}&${building}=${this.data.choosedBuilding}&index=${index}`;
-    _this.setData({
-      data: 0
-    })
     let url = `${app.globalData.commonUrl}${availableSuffix}${available}`;
     let header = getHeader("urlencoded", app.globalData.token);
-    let data = {};
-    let data_initial = {};
-    let data_content = _this.data.data_content
-    _this.setData({
-      data_content:[]
-    })
-    let tapDate = requestUtils.doGET(url, data, header);
-    tapDate.then((res) => {
-      data_initial = res.data.data.rooms
-      for (var i = 0; i < data_initial.length; i++) {
-        let datas = data_initial[i];
-        datas.busyTime = transformationsUtils.transformations(datas.busyTime,11); 
-      }
-      data_content = data_initial
+    let dataInitial = {};
+    let content = _this.data.content;
+    let sendData = requestUtils.doGET(url, content, header);
+    let dataContent = {};
+    sendData.then((res) => {
+      dataInitial = res.data.data.rooms;
+      dataInitial.map((el) => {
+        el.busyTime = transformationsUtils.transformations(el.busyTime, 11);
+      });
+      index != 1
+        ? (dataContent = content.concat(dataInitial))
+        : (dataContent = dataInitial);
       _this.setData({
-        data: data_content
-      })
-    })
+        content: dataContent,
+      });
+    });
   },
-
-  loadmore: function(){
+  //上拉加载
+  loadMore() {
     let _this = this;
-    setTimeout(function(){
-      console.log('上拉加载更多');
-      let pages = _this.data.index;
-      pages = pages+1;
-      _this.setData({
-        index: pages,
-        hideBottom:false
-      })
-      let campus = _this.Choosed()[0];
-      let building = _this.Choosed()[1];
-      let index = _this.data.index;
-      let available = `?campus=${campus}&date=${_this.data.choosedDate}&${building}=${_this.data.choosedBuilding}&index=${index}`;
-      let url = `${app.globalData.commonUrl}${availableSuffix}${available}`;
-      let header = getHeader("urlencoded", app.globalData.token);
-      let data = {};
-      let data_initial = {};
-      let data_content = _this.data.data_content;
-      let tapDate = requestUtils.doGET(url, data, header);
-      tapDate.then((res) => {
-        data_initial = res.data.data.rooms
-        for (var i = 0; i < data_initial.length; i++) {
-          let datas= data_initial[i];
-          datas.busyTime = transformationsUtils.transformations(datas.busyTime,11); 
-          data_initial[i] = datas
-        }
-        if(Array.isArray(data_content)){
-        data_content=data_content.concat(data_initial);}
-        _this.setData({
-          data: data_content,
-          data_content:data_content,
-          hideBottom :true
-        })
-      })
-    },100);
-
-
+    console.log("上拉加载更多");
+    let pages = _this.data.index;
+    pages = pages + 1;
+    _this.setData({
+      index: pages,
+    });
+    _this.sendData(
+      _this.Choosed()[0],
+      _this.Choosed()[1],
+      pages,
+      _this.data.choosedDate
+    );
   },
-
-  onLoad: function(options) {
-    let _this = this
-    let now = dayjs()
-    let today = now.startOf('day')
-    let dates = []
+  //渲染开始
+  onLoad: function (options) {
+    let _this = this;
     let index = _this.data.index;
-    for (let i of [0, 1, 2, 3, 4, 5, 6]) {
-      let day = today.add(i, 'day')
-      dates.push(day.format('YYYY-MM-DD'))
-    }
     let campus = _this.Choosed()[0];
     let building = _this.Choosed()[1];
-    _this.setData({
-      data: 0
-    })
-    this.setdata(campus,building,index,dates[0])
-  },
-
-  onShow: function() {
-    let now = dayjs()
-    let today = now.startOf('day')
-    let nowInterval = 0
-    for (let i in timeIntervals) {
-      let startTime = today.add(timeIntervals[i][0].h, 'hour').add(timeIntervals[i][0].m, 'minute')
-      let endTime = today.add(timeIntervals[i][1].h, 'hour').add(timeIntervals[i][1].m, 'minute')
-      if (now.isAfter(startTime) && now.isBefore(endTime)) {
-        nowInterval = i
-        break
-      }
+    let today = Date.parse(new Date());
+    let dates = timeUtils.getTimeOfHebdomad(today);
+    let weekDates = [];
+    for (let i = 0; i < dates.length; i++) {
+      weekDates[i] = dates[i].year + "-" + dates[i].month + "-" + dates[i].day;
     }
-
-    let dates = []
-    for (let i of [0, 1, 2, 3, 4, 5, 6]) {
-      let day = today.add(i, 'day')
-      dates.push(day.format('YYYY-MM-DD'))
-    }
-
-    let choosedDate = dates[0]
-
-    this.setData({ dates, choosedDate, nowInterval })
+    let choosedDate = weekDates[0];
+    this.sendData(campus, building, index, weekDates[0]);
+    this.setData({ weekDates, choosedDate });
   },
 
-  tapDate: function(event) {
-    let _this = this
-    const date = event.currentTarget.dataset.date
-    this.setData({ choosedDate: date })
-    let pages = _this.data.index;
-    pages=0;
+  onShow: function () {},
+  //选择时间
+  tapDate: function (event) {
+    let _this = this;
+    const date = event.currentTarget.dataset.date;
     _this.setData({
-      index :pages
-    })
-    let campus = _this.Choosed()[0];
-    let building = _this.Choosed()[1];
-    let Date = this.data.choosedDate;
-    this.setdata(campus,building,pages,Date)
+      index: 1,
+      choosedDate: date,
+    });
+    this.sendData(
+      _this.Choosed()[0],
+      _this.Choosed()[1],
+      1,
+      this.data.choosedDate
+    );
   },
-
-  tapCampus: function(event) {
-    let _this = this
-    const campus = event.currentTarget.dataset.campus
-    this.setData({ choosedCampus: campus, choosedBuilding: buildings[campus][0] })
-    let pages = _this.data.index;
-    pages=0;
-    _this.setData({
-      index :pages
-    })
-    let Campus = _this.Choosed()[0];
-    let building = _this.Choosed()[1];
-    let date = _this.data.choosedDate;
-    this.setdata(Campus,building,pages,date)
+  //选择校区
+  tapCampus: function (event) {
+    let _this = this;
+    const campus = event.currentTarget.dataset.campus;
+    this.setData({
+      choosedCampus: campus,
+      choosedBuilding: buildings[campus][0],
+      index: 1,
+    });
+    this.sendData(
+      _this.Choosed()[0],
+      _this.Choosed()[1],
+      1,
+      _this.data.choosedDate
+    );
   },
-
-  tapBuilding: function(event) {
-    let _this = this
+  //选择教学楼
+  tapBuilding: function (event) {
+    let _this = this;
     const building = event.currentTarget.dataset.building;
-    this.setData({ choosedBuilding: building });
-    let pages = _this.data.index;
-    pages=0;
     _this.setData({
-      index :pages
-    })
-    let campus = _this.Choosed()[0];
-    let Building = _this.Choosed()[1];
-    let date = _this.data.choosedDate;
-    this.setdata(campus,Building,pages,date)
-  }
-})
-
+      index: 1,
+      choosedBuilding: building,
+    });
+    this.sendData(
+      _this.Choosed()[0],
+      _this.Choosed()[1],
+      1,
+      _this.data.choosedDate
+    );
+  },
+});
--- utils/timeUtils.js
+++ utils/timeUtils.js
@@ -1,7 +1,7 @@
 // getTimeStamp(GMTTime: String): unixTimeStamp: Number
 // 返回13位的时间戳(精确到毫秒)
 // 格式为"2020-08-31T16:18:57"
-const getTimeStamp = GMTTime => {
+const getTimeStamp = (GMTTime) => {
   const arr = GMTTime.split("T");
   const str = arr[0] + " " + arr[1];
   return new Date(str).getTime();
@@ -10,8 +10,7 @@ const getTimeStamp = GMTTime => {
 // getIntervalToCurrentTime(GivenTime: String): intervalTime: String
 // 获取给定时间到现在的时间间隔
 // 返回形式为 XX年XX月XX日XX:XX:XX
-const getIntervalToCurrentTime = givenTime => {
-
+const getIntervalToCurrentTime = (givenTime) => {
   if (null == givenTime) return null;
 
   var givenTimeStamp = getTimeStamp(givenTime) / 1000;
@@ -28,47 +27,40 @@ const getIntervalToCurrentTime = givenTime => {
   var intervalTime;
   var tmp;
 
-
   if (interval < 60) {
     intervalTime = "1分钟";
-  }
-  else if (interval < 3600) {
+  } else if (interval < 3600) {
     tmp = parseInt(interval / 60);
     intervalTime = tmp + "分钟";
-  }
-  else if (interval < 3600 * 24) {
+  } else if (interval < 3600 * 24) {
     tmp = parseInt(interval / 3600);
     intervalTime = tmp + "小时";
-  }
-  else if (interval < 3600 * 24 * 30) {
+  } else if (interval < 3600 * 24 * 30) {
     tmp = parseInt(interval / (3600 * 24));
     intervalTime = tmp + "天";
-  }
-  else if (interval < 3600 * 24 * 30 * 12) {
+  } else if (interval < 3600 * 24 * 30 * 12) {
     tmp = parseInt(interval / (3600 * 24 * 30));
     intervalTime = tmp + "月";
-  }
-  else {
+  } else {
     tmp = parseInt(interval / (3600 * 24 * 30 * 12));
     intervalTime = tmp + "年";
   }
   return intervalTime;
-
 };
 
 // formatNumber(n: Number): nn: String
-const formatNumber = n => {
+const formatNumber = (n) => {
   n = n.toString();
   return n[1] ? n : `0${n}`;
 };
 
 // formatTime(date: Date): YYYY-MM-dd
-const formatTime = date => {
+const formatTime = (date) => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
-  return [year, month, day].map(formatNumber).join('-');
+  return [year, month, day].map(formatNumber).join("-");
 };
 
 // getIntervalToCurrentWeek(Giventime: String): intervalWeek : number
@@ -79,19 +71,24 @@ const getIntervalToCurrentWeek = (givenTime, giveTime) => {
   var CurrentTime = giveTime;
   let intervalWeek = Math.ceil((CurrentTime - startTime) / 604800000);
   return intervalWeek;
-}
+};
 
 // getTimeOfWeek(Giventime: String): intervalWeek :array
 // 获得给定时间对应一周的日期和星期
 // 返回格式 [{ year: yyyy, month: MM, day: dd, week: number }]
-const getTimeOfWeek = givenTime => {
+const getTimeOfWeek = (givenTime) => {
   if (null == givenTime) return null;
-  let weekday
+  let weekday;
   let week = new Date(givenTime).getDay();
   let nowDate = givenTime;
-  let newDate = 0,day = 0,year = 0,month = 0
-  let intervalWeek = []
-  if (week === 0) {week = 7}
+  let newDate = 0,
+    day = 0,
+    year = 0,
+    month = 0;
+  let intervalWeek = [];
+  if (week === 0) {
+    week = 7;
+  }
   for (let i = 0; i < week - 1; i++) {
     newDate = nowDate - 86400000 * (week - 1 - i);
     newDate = new Date(newDate);
@@ -100,8 +97,11 @@ const getTimeOfWeek = givenTime => {
     month = newDate.getMonth() + 1;
     weekday = newDate.getDay();
     intervalWeek.push({
-      year, month, day, week : weekday
-    })
+      year,
+      month,
+      day,
+      week: weekday,
+    });
   }
   for (let i = week; i < 8; i++) {
     newDate = nowDate + 86400000 * (i - week);
@@ -111,16 +111,51 @@ const getTimeOfWeek = givenTime => {
     month = newDate.getMonth() + 1;
     weekday = newDate.getDay();
     intervalWeek.push({
-      year, month, day, week : weekday
-    })
+      year,
+      month,
+      day,
+      week: weekday,
+    });
   }
   return intervalWeek;
-}
-
+};
+// getTimeOfHebdomad(Giventime: String): intervalWeek :array
+// 获得给定时间后七天的日期和星期
+// 返回格式 [{ year: yyyy, month: MM, day: dd, week: number }]
+const getTimeOfHebdomad = (givenTime) => {
+  if (null == givenTime) return null;
+  let weekday;
+  let week = new Date(givenTime).getDay();
+  let nowDate = givenTime;
+  let newDate = 0,
+    day = 0,
+    year = 0,
+    month = 0;
+  let intervalWeek = [];
+  if (week === 0) {
+    week = 7;
+  }
+  for (let i = 0; i < 7; i++) {
+    newDate = nowDate + 86400000 * i;
+    newDate = new Date(newDate);
+    day = newDate.getDate();
+    year = newDate.getFullYear();
+    month = newDate.getMonth() + 1;
+    weekday = newDate.getDay();
+    intervalWeek.push({
+      year,
+      month,
+      day,
+      week: weekday,
+    });
+  }
+  return intervalWeek;
+};
 module.exports = {
   getTimeStamp,
   getIntervalToCurrentTime,
   formatTime,
   getIntervalToCurrentWeek,
-  getTimeOfWeek
+  getTimeOfWeek,
+  getTimeOfHebdomad,
 };
