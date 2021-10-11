b243ccefae6a94310d5ce2861f33ffb97e9c94d7
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2021-10-11 19:33:42 +0800

    合并课表更改

--- score/components/card/card.js
+++ score/components/card/card.js
@@ -10,8 +10,21 @@ Component({
     credit: {
       type:String,
     },
+    isRequiredCourse: {
+      type:Boolean,
+    },
+    semester: {
+      type: String,
+    },
     grades: {
       type:String
+    },
+    isFolded: {
+      type: Boolean,
+      value: true
+    },
+    detailList: {
+      type: Array
     }
   },
 
@@ -26,6 +39,28 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onTap() {
+      this.triggerEvent('bindCard')
+      if(this.data.isFolded) {
+        this.setData({isFolded: false})
+        this.animation.scale(1, 1).step()
+        this.setData({animation: this.animation.export()})
+      }else {
+        // this.animation.scale(1, 0).step()
+        //   this.setData({animation: this.animation.export()})
+        //   setTimeout(() => {
+            this.setData({isFolded: true})
+          // }, 100);
+      }
+    }
+  },
+  lifetimes: {
+    attached: function() {
+      // 在组件实例进入页面节点树时执行
+      this.animation = wx.createAnimation({
+        duration: 1000,
+        timingFunction: 'ease'
+      })
+    },
   }
 })
