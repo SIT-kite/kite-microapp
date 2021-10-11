7f4553576d7fbfe1de538d7016a8cd3eae3240bf
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2021-10-11 19:02:58 +0800

    合并 utils 更改

--- utils/type.js
+++ utils/type.js
@@ -1,25 +1,44 @@
 // type.js
 // getType(something): type: String
-const getType = _ => Object.prototype.toString.call(_).slice(8, -1);
+const getType = value => Object.prototype.toString.call(value).slice(8, -1);
 
-const isNonEmptyArray = _ => (
-	Array.isArray(_) &&
-	_.length > 0
+const isNonEmptyArray = value => (
+	Array.isArray(value) &&
+	value.length > 0
 );
 
-const isNonEmptyString = _ => (
-	getType(_) === "String" &&
-	_ !== ""
+const isNonEmptyString = value => (
+	getType(value) === "String" &&
+	value !== ""
 );
 
-const isNonBlankString = _ => (
-	getType(_) === "String" &&
-	String.prototype.trim.call(_) !== ""
+const isNonBlankString = value => (
+	getType(value) === "String" &&
+	String.prototype.trim.call(value) !== ""
 );
+
+const check = (
+  value, type, opt // has, length, startsWith, endsWith, contains
+) => {
+  if (getType(value) !== type) {
+    return false;
+  } else switch (type) {
+    case "Object": {
+      const hasOwn = prop => Object.prototype.hasOwnProperty.call(value, prop);
+      switch (getType(opt.has)) {
+        case "String": return hasOwn(opt.has);
+        case "Array": return opt.has.every(hasOwn);
+        default: return true;
+      }
+    }
+    default: return true;
+  }
+};
 
 export {
 	getType,
 	isNonEmptyArray,
 	isNonEmptyString,
-	isNonBlankString
+  isNonBlankString,
+  check
 };
--- utils/updateManager.js
+++ utils/updateManager.js
@@ -9,18 +9,20 @@ export default () => {
 		res => console.log(res.hasUpdate ? "有新版本！" : "暂无新版本")
 	);
 
-	updateManager.onUpdateReady(() => {
-		wx.showModal({
+	updateManager.onUpdateReady(
+		// 微信已支持更新提示，不需要再弹对话框
+		() => updateManager.applyUpdate()
+		/* () => wx.showModal({
 			title: "更新提示",
-			content: "新版本已经准备好，是否重启应用？"
+			content: "小程序本次更新将清空数据，更新后重新使用微信登录即可，数据将在登陆后同步。"
 		}).then(res => {
 			if (res.confirm) {
 				// 清空本地存储；清空后用户将重新登录，新版本与旧版本存储不兼容时使用
-				// wx.clearStorageSync();
+				wx.clearStorageSync();
 				updateManager.applyUpdate();
 			}
-		});
-	});
+		}) */
+	);
 
 	updateManager.onUpdateFailed(() => {
 		console.warn("新版本下载失败");
