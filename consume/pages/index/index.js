1a0a718b0d1316c71ad2516186eabaed9c32540f
pony-zhang <sherly_cs_sh@163.com>
2021-11-03 13:37:57 +0800

    修复进入 “ 消费 ” 未触发爬虫

--- consume/pages/index/index.js
+++ consume/pages/index/index.js
@@ -175,14 +175,14 @@ Page({
                         })
                     })
                   }, 10000)
-                })
+                })()
               : (() => {
                   this.fetchData('1', res => {
                     this.setData({list: res})
                     wx.hideLoading();
                   })
                   this.triggerSpider('2');
-                })
+                })()
           : (() => {
             this.triggerSpider('2');
             this.setData({isShowLoading: true})
@@ -198,52 +198,7 @@ Page({
                   })
               })
             }, 10000)
-          })
+          })()
           })()
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    // onReady: function () {
-    //
-    // },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    // onShow: function () {
-    //
-    // },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    // onHide: function () {
-    //
-    // },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    // onUnload: function () {
-    //
-    // },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    // onPullDownRefresh: function () {}
-    // ,
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    // onReachBottom: function () {
-    //
-    // },
-
-    /**
-     * 用户点击右上角分享
-     */
 })
